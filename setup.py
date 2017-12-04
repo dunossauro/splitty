fdbbefed9935ee49d1ceed1e1fbb2da43bedce5e
z4r4tu5tr4 <mendesxeduardo@gmail.com>
2017-12-04 00:04:20 -0200

    FIX github page on pipy

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 
 setup(name='splitty',
-      version='0.0.4',
+      version='0.0.5',
       description='functional approach to work with iterables in python',
       url='https://github.com/z4r4tu5tr4/splitty',
       author='Eduardo Mendes',
