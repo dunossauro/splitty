4727f80603b194736685f2393f4253fd779b2c77
z4r4tu5tr4 <mendesxeduardo@gmail.com>
2017-12-03 23:50:14 -0200

    new version to imports

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 
 setup(name='splitty',
-      version='0.0.2',
+      version='0.0.3',
       description='functional approach to work with iterables in python',
       url='https://github.com/z4r4tu5tr4/splitter',
       author='Eduardo Mendes',
