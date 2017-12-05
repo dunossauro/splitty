1eb8ce905f3b5057ef879070e203acf4f2cf2090
rg3915 <rg3915@yahoo.com.br>
2017-12-04 23:58:48 -0200

    TestListByRePattern

--- tests/test_splitty.py
+++ tests/test_splitty.py
@@ -1,3 +1,4 @@
+import re
 from unittest import TestCase, main
 from splitty.splitty import (list_by_list, list_by_re_pattern,
                              make_intervals, apply_list_intervals)
@@ -9,6 +10,18 @@ class TestListByList(TestCase):
         list_to_be_splited = ['spam', 1, 2, 3, 'eggs', 1, 2, 3, 'foo', 1, 2, 3]
         split_by = ['spam', 'eggs', 'foo']
         result = list_by_list(list_to_be_splited, split_by)
+        assert result == [(0, 'spam'), (4, 'eggs'), (8, 'foo')]
+
+
+class TestListByRePattern(TestCase):
+
+    def test_list_by_re_pattern(self):
+        list_to_be_splited = ['spam', '1', '2', '3',
+                              'eggs', '1', '2', '3',
+                              'foo', '1', '2', '3']
+        split_by = r'(spam|eggs|f.o)'
+        result = list_by_re_pattern(list_to_be_splited, split_by)
+        print(result)
         assert result == [(0, 'spam'), (4, 'eggs'), (8, 'foo')]
 
 
