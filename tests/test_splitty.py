2a781e95d82cb1ec8f5456eaceeaae88362f9ed2
rg3915 <rg3915@yahoo.com.br>
2017-12-04 23:56:47 -0200

    TestListByList

---
+++ tests/test_splitty.py
@@ -0,0 +1,15 @@
+from unittest import TestCase, main
+from splitty.splitty import (list_by_list, list_by_re_pattern,
+                             make_intervals, apply_list_intervals)
+
+
+class TestListByList(TestCase):
+
+    def test_list_by_list(self):
+        list_to_be_splited = ['spam', 1, 2, 3, 'eggs', 1, 2, 3, 'foo', 1, 2, 3]
+        split_by = ['spam', 'eggs', 'foo']
+        result = list_by_list(list_to_be_splited, split_by)
+        assert result == [(0, 'spam'), (4, 'eggs'), (8, 'foo')]
+
+
+main()
