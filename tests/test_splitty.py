d64aee6eb7b23aad4f0289652ce2d807115ee73a
z4r4tu5tr4 <mendesxeduardo@gmail.com>
2017-12-07 13:34:25 -0200

    unittests self

--- tests/test_splitty.py
+++ tests/test_splitty.py
@@ -8,8 +8,9 @@ class TestListByList(TestCase):
     def test_list_by_list(self):
         list_to_be_splited = ['spam', 1, 2, 3, 'eggs', 1, 2, 3, 'foo', 1, 2, 3]
         split_by = ['spam', 'eggs', 'foo']
-        result = list_by_list(list_to_be_splited, split_by)
-        assert result == [(0, 'spam'), (4, 'eggs'), (8, 'foo')]
+
+        self.assertEqual(list_by_list(list_to_be_splited, split_by),
+                         [(0, 'spam'), (4, 'eggs'), (8, 'foo')])
 
 
 class TestListByRePattern(TestCase):
@@ -18,8 +19,9 @@ class TestListByRePattern(TestCase):
                               'eggs', '1', '2', '3',
                               'foo', '1', '2', '3']
         split_by = r'(spam|egg(s|z)|f.o)'
-        result = list_by_re_pattern(list_to_be_splited, split_by)
-        assert result == [(0, 'spam'), (4, 'eggs'), (8, 'foo')]
+        self.assertEqual(list_by_re_pattern(list_to_be_splited, split_by),
+                         [(0, 'spam'), (4, 'eggs'), (8, 'foo')])
 
 
-main()
+if __name__ == '__main__':
+    main()
