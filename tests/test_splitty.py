711eea68a442506b7264e3780c6d1b0bc58f1313
z4r4tu5tr4 <mendesxeduardo@gmail.com>
2018-01-03 21:48:38 -0200

    make_intervals tests

--- tests/test_splitty.py
+++ tests/test_splitty.py
@@ -3,7 +3,8 @@
 from sys import path
 from unittest import TestCase, main
 path.append('./splitty')
-from splitty import list_by_re_pattern, find_elements, list_by_list
+from splitty import (list_by_re_pattern, find_elements,
+                     list_by_list, make_intervals)
 
 
 class TestFindListElements(TestCase):
@@ -45,6 +46,29 @@ class TestListByRePattern(TestCase):
                               'foo', '1', '2', '3']
         self.assertEqual(list_by_re_pattern(list_to_be_splited, split_by),
                          [(0, 'spam'), (4, 'eggs'), (8, 'foo')])
+
+
+class Test_make_intervals(TestCase):
+    def test_make_intervals(self):
+        _list = list('abcdefg')
+        list_to_split = list('ce')
+
+        topics = find_elements(_list, list_to_split)
+
+        self.assertEqual(make_intervals(topics),
+                         [slice(2, 4, None),
+                          slice(4, None, None)])
+
+    def test_make_intervals_has_element_zero(self):
+        _list = list('abcdefg')
+        list_to_split = list('ce')
+
+        topics = find_elements(_list, list_to_split)
+
+        self.assertEqual(make_intervals(topics, start=True),
+                         [slice(0, 2, None),
+                          slice(2, 4, None),
+                          slice(4, None, None)])
 
 
 if __name__ == '__main__':
