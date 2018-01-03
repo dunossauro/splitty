8bf758aab67c49f1a7a9d1e2fa0517dc925ff440
z4r4tu5tr4 <mendesxeduardo@gmail.com>
2018-01-03 21:49:44 -0200

    Make intervals feature close #3

--- setup.py
+++ setup.py
@@ -1,8 +1,7 @@
 from setuptools import setup
 
-
 setup(name='splitty',
-      version='0.0.6',
+      version='0.0.7',
       description='functional approach to work with iterables in python',
       url='https://github.com/z4r4tu5tr4/splitty',
       author='Eduardo Mendes',
