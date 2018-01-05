cd18c21f10299f47b6880bede6c378cf6b8e2f93
z4r4tu5tr4 <mendesxeduardo@gmail.com>
2018-01-05 15:10:08 -0200

    Version 0.0.8; Add: Chunks function A solve #2 with argument

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='splitty',
-      version='0.0.7',
+      version='0.0.8',
       description='functional approach to work with iterables in python',
       url='https://github.com/z4r4tu5tr4/splitty',
       author='Eduardo Mendes',
