ed8731874984ae54559fa0635b0dd7c08f4c4666
z4r4tu5tr4 <mendesxeduardo@gmail.com>
2018-01-23 18:03:48 -0200

    Examples in tox; Simplify example

--- examples/book_parsed/parse_a_book.py
+++ examples/book_parsed/parse_a_book.py
@@ -1,43 +1,38 @@
-from operator import getitem
-from pprint import pprint
-from splitty import list_by_re_pattern, list_by_list, clear_list_strings
+from splitty import (list_by_re_pattern, apply_intervals,
+                     clear_list_strings, make_intervals)
 
-with open('examples/mobydick.txt') as text:
+with open('examples/book_parsed/mobydick.txt') as text:
     book = clear_list_strings(text.read().split('\n'))
 
 numered_topics = list_by_re_pattern(book, r'CHAPTER \d{1,3}\. .*')
-"""Result.
-    [(47, 'CHAPTER 1. Loomings.'),
-     (49, 'CHAPTER 2. The Carpet-Bag.'),
-     (51, 'CHAPTER 3. The Spouter-Inn.'),
-     (53, 'CHAPTER 4. The Counterpane.'),
-     (55, 'CHAPTER 5. Breakfast.'), ... ,]
 """
-topics = [getitem(x, 1) for x in numered_topics]
-
-pprint(topics)
-"""
-Topics
-'CHAPTER 1. Loomings.',
- 'CHAPTER 2. The Carpet-Bag.',
- 'CHAPTER 3. The Spouter-Inn.',
- 'CHAPTER 4. The Counterpane.',
- 'CHAPTER 5. Breakfast.',
- 'CHAPTER 6. The Street.',
- ...
- 'CHAPTER 135. The Chase.—Third Day.
+[(0, 'CHAPTER 1. Loomings.'),
+ (184, 'CHAPTER 2. The Carpet-Bag.'),
+ (304, 'CHAPTER 3. The Spouter-Inn.'),
+ (798, 'CHAPTER 4. The Counterpane.'),
+ (933, 'CHAPTER 5. Breakfast.'),
+ (1000, 'CHAPTER 6. The Street.'),
+ (1074, 'CHAPTER 7. The Chapel.'),
+ (1156, 'CHAPTER 8. The Pulpit.'),
+ (1239, 'CHAPTER 9. The Sermon.'),
+ (1537, 'CHAPTER 10. A Bosom Friend.'),
 """
 
-pprint(list_by_list(book, topics)[-1])
+full_book_chapter_lists = apply_intervals(book,
+                                          make_intervals(numered_topics))
+
+chapter_1 = ' '.join(full_book_chapter_lists[0])
+
+print(chapter_1)
 """
-'CHAPTER 135. The Chase.—Third Day.',
- 'The morning of the third day dawned fair and fresh, and once more the',
- 'solitary night-man at the fore-mast-head was relieved by crowds of the',
- 'daylight look-outs, who dotted every mast and almost every spar.',
- ...
-  '  http://www.gutenberg.org',
- 'This Web site includes information about Project Gutenberg-tm,',
- 'including how to make donations to the Project Gutenberg Literary',
- 'Archive Foundation, how to help produce our new eBooks, and how to',
- 'subscribe to our email newsletter to hear about new eBooks.']
- """
+CHAPTER 1. Loomings. Call me Ishmael. ... like a snow hill in the air.
+"""
+
+last_chapter = ' '.join(full_book_chapter_lists[-1])
+
+print(last_chapter)
+"""
+CHAPTER 135. The Chase.—Third Day. The morning of the third day dawned fair
+...
+*** END OF THIS PROJECT GUTENBERG EBOOK MOBY DICK; OR THE WHALE ***
+"""
