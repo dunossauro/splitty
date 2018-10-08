978b419c68aaeb71870cd2d72d37196b3102ee5b
Serrones <fabioserrones@gmail.com>
2018-10-07 23:55:04 -0300

    Comments and examples fixed

--- examples/book_parsed/parse_a_book.py
+++ examples/book_parsed/parse_a_book.py
@@ -1,13 +1,17 @@
 from splitty import (list_by_re_pattern, apply_intervals,
                      clear_list_strings, make_intervals)
 
-# Open moby dick book
+
+# Open Moby Dick book
 with open('examples/book_parsed/mobydick.txt') as text:
-    # book recive a non-blanklines strings
+    # book receives non-blanklines strings
     book = clear_list_strings(text.read().split('\n'))
 
-# Find re ocurrences (using a regex) in all line lists
+# Find text ocurrences using a regex pattern in all book lines
 numered_topics = list_by_re_pattern(book, r'CHAPTER \d{1,3}\. .*')
+
+# Print topic's list
+print(numered_topics)
 """
 [(0, 'CHAPTER 1. Loomings.'),
  (184, 'CHAPTER 2. The Carpet-Bag.'),
@@ -15,32 +19,34 @@ numered_topics = list_by_re_pattern(book, r'CHAPTER \d{1,3}\. .*')
  (798, 'CHAPTER 4. The Counterpane.'),
  (933, 'CHAPTER 5. Breakfast.'),
  (1000, 'CHAPTER 6. The Street.'),
- (1074, 'CHAPTER 7. The Chapel.'),
- (1156, 'CHAPTER 8. The Pulpit.'),
- (1239, 'CHAPTER 9. The Sermon.'),
- (1537, 'CHAPTER 10. A Bosom Friend.'),
+ ...
+ ...
+ (17687, 'CHAPTER 134. The Chase—Second Day.'),
+ (17976, 'CHAPTER 135. The Chase.—Third Day.')]
 """
 
-# Use regex finded topics and make a interval lists
-# somithng like [slice(0, 184), slice(184, 304) ...]
+# Use regex to find topics and make a interval lists
+# Something like '[slice(0, 184), slice(184, 304) ...]'
 # And apply this intervals in all book lines
 full_book_chapter_lists = apply_intervals(book,
                                           make_intervals(numered_topics))
 
-# join first chapter lists
+# Join first chapter's list
 chapter_1 = ' '.join(full_book_chapter_lists[0])
 
+# Print First Chapter
 print(chapter_1)
 """
-CHAPTER 1. Loomings. Call me Ishmael. ... like a snow hill in the air.
+CHAPTER 1. Loomings. Call me Ishmael. Some years ago—never mind...
+...of them all, one grand hooded phantom, like a snow hill in the air.
 """
 
+# Join last chapter's list
 last_chapter = ' '.join(full_book_chapter_lists[-1])
 
-# join last chapter lists
+# Print First Chapter
 print(last_chapter)
 """
-CHAPTER 135. The Chase.—Third Day. The morning of the third day dawned fair
-...
-*** END OF THIS PROJECT GUTENBERG EBOOK MOBY DICK; OR THE WHALE ***
+CHAPTER 135. The Chase.—Third Day. The morning of the third day...
+...END OF THIS PROJECT GUTENBERG EBOOK MOBY DICK; OR THE WHALE ***
 """
