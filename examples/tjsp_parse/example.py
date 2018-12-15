f76db75b75126f489ddc5da8b6676870ace41f6e
dunossauro <mendesxeduardo@gmail.com>
2018-12-15 01:27:29 -0200

    tjsp example using apply intervals

--- examples/tjsp_parse/example.py
+++ examples/tjsp_parse/example.py
@@ -1,6 +1,11 @@
 from json import dump
 from re import search
-from splitty import list_by_re_pattern, make_intervals
+from splitty import (
+    list_by_re_pattern,
+    make_intervals,
+    apply_intervals,
+    clear_list_strings,
+)
 
 regex_process = (
     r'^Processo \d+-(\d+\.?){5}(/\d{2})?\s?(\(Processo \d+-(\d+\.?){5}\))?'
@@ -17,32 +22,30 @@ def get_process_number(vara, block):
 
 
 with open('examples/tjsp_parse/caderno3_parsed.txt') as f:
-    file = [string.strip() for string in f.readlines() if string.strip()]
+    file = clear_list_strings(f.readlines())
 
-vila_prudente_block = make_intervals(
-    list_by_re_pattern(file, r'(IX - Vila Prudente|X - Ipiranga)')
-)  # [slice(5, 4862, None), slice(4862, None, None)]
+vila_prudente = apply_intervals(
+    file,
+    make_intervals(
+        list_by_re_pattern(file, r'(IX - Vila Prudente|X - Ipiranga)')
+    ),
+)[0]
 
-vila_prudente = file[vila_prudente_block[0]]
-
-varas_blocks = make_intervals(
-    list_by_re_pattern(
-        vila_prudente, r'^(Juizado Especial|\dª\sVara)\sCível\n$'
-    )
-)  # [slice(2, 270, None), slice(270, 921, None), slice(921, 2187, None), slice(2187, 4273, None), slice(4273, None, None)] # NOQA
-
-vara_1 = vila_prudente[varas_blocks[0]]
+vara_1 = apply_intervals(
+    vila_prudente,
+    make_intervals(
+        list_by_re_pattern(
+            vila_prudente, r'^(Juizado Especial|\dª\sVara)\sCível\n$'
+        )
+    ),
+)[0]
 
 processo_blocks = make_intervals(list_by_re_pattern(vara_1, regex_process))
 
-processos_1 = {
+processos_vara_1 = {
     get_process_number(vara_1, processo): ' '.join(vara_1[processo])
     for processo in processo_blocks
 }
 
-# import ipdb; ipdb.set_trace()
-
-# result = {}
-
 with open('examples/tjsp_parse/caderno3_parsed.json', 'w') as f:
-    dump(processos_1, f, indent=2, ensure_ascii=False, sort_keys=True)
+    dump(processos_vara_1, f, indent=2, ensure_ascii=False, sort_keys=True)
