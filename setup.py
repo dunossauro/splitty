6b002580282eca19442f60940420f5421e9741b1
dunossauro <mendesxeduardo@gmail.com>
2019-04-05 00:00:04 -0300

    v0.0.10 and black on setup

--- setup.py
+++ setup.py
@@ -1,11 +1,13 @@
 from setuptools import setup
 
-setup(name='splitty',
-      version='0.0.9',
-      description='functional approach to work with iterables in python',
-      url='https://github.com/dunossauro/splitty',
-      author='Eduardo Mendes',
-      author_email='mendesxeduardo@gmail.com',
-      license='MIT',
-      packages=['splitty'],
-      zip_safe=False)
+setup(
+    name='splitty',
+    version='0.0.10',
+    description='functional approach to work with iterables in python',
+    url='https://github.com/dunossauro/splitty',
+    author='Eduardo Mendes',
+    author_email='mendesxeduardo@gmail.com',
+    license='MIT',
+    packages=['splitty'],
+    zip_safe=False,
+)
